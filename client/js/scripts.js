2cbc298deefba6a5976550d48754f2fe7b8df06a
shuvam <shuvam86>
2022-07-11 00:16:34 +0530

    inital commit

---
+++ client/js/scripts.js
@@ -0,0 +1,22 @@
+// // function getAlert() {
+// // 	alert('Jn');
+// // }
+
+// function getAlert() {
+// 	var person1= {name:"Abcd", age:25};
+// 	// document.getElementById('demo').innerHTML= "person1.age";
+// 	// document.getElementById('demo').innerHTML= person1.name+" is "+ person1.age;
+// 	var person2= {name:"Efgh", age:26};
+// 	var person3= {name:"Ijkl", age:27};
+
+// 	var people= [person1,person2,person3];
+// 	// console.log(people);
+// 	// document.write(people);
+
+// 	for(var i=0;i<people.length;i++) {
+// 		document.getElementById('demo').innerHTML+= people[i].name + "<br>" ;
+// 	}
+
+// }
+
+
